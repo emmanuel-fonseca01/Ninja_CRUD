3b97fd26b5a8e7d7de1b1edf4392d2695f7bc4ca
Emmanuel Fonseca Mora <emmafonsemo@gmail.com>
2024-11-09 20:03:29 -0600

    Implement read endpoints (GET) for single and list data retrieval

--- ninja_api_crud/api/views.py
+++ ninja_api_crud/api/views.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 from .models import Book
-from .serializers import BookSchema
+from .serializers import BookSchema, BookSchemaOut
+from typing import List
+from django.shortcuts import get_object_or_404
 
 api = NinjaAPI()
 
@@ -8,3 +10,15 @@ api = NinjaAPI()
 def create_book(request, payload: BookSchema):
     book = Book.objects.create(**payload.dict())
     return {"id": book.id}
+
+
+@api.get("/books/{book_id}", response=BookSchemaOut)
+def get_book(request, book_id: int):
+    book = get_object_or_404(Book, id=book_id)
+    return book
+
+
+@api.get("/books", response=List[BookSchemaOut])
+def list_books(request):
+    qs = Book.objects.all()
+    return qs
